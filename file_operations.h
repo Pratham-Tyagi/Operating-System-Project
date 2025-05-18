8654b4642a3ac8e78556c42c9956f1a6eb0d708c
Pratham-Tyagi <tyagipratham26@gmail.com>
2025-05-18 19:25:48 +0530

    Create file_operations.h

---
+++ file_operations.h
@@ -0,0 +1,11 @@
+#ifndef FILE_OPERATIONS_H
+#define FILE_OPERATIONS_H
+
+void createFile();
+void writeToFile();
+void modifyFile();
+void searchContent();
+void readFromFile();
+void deleteFile();
+
+#endif
